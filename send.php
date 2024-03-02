46c88c1c6a7c704361e62679f8945069e8655e24
Rafik <rfkabubakari@gmail.com>
2024-03-02 11:25:43 +0000

    changed

--- send.php
+++ send.php
@@ -1,59 +1,39 @@
+
 <?php
 
-use PHPMailer\PHPMailer\PHPmailer;
+use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPmailer.php';
 require 'phpmailer/src/SMTP.php';
-if (isset($_POST['send']))
-{
-$mail= new PHPMailer(true);
-$mail->isSMTP();
-$mail->Host=smtp.gmail.com;
-$mail->SMTPAuth=true;
-$mail->Username='office.consoleghana@gmail.com';
-$mail->password='swcd juwy wlyf wtrl';
-$mail->SMTPSecure='ssl';
-$mail->PORT='465';
 
+if (isset($_POST['send'])) {
+    $mail = new PHPMailer(true);
 
-$mail->setFrom('office.consoleghana@gmail.com');
-$mail->AddAddress($_POST['email']);
-$mail->isHTML(true);
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'office.consoleghana@gmail.com'; // Use environment variable for username
+        $mail->Password = 'swcd juwy wlyf wtrl'; // Use environment variable for password
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
 
-$mail->Subject=$_POST('subject');
-$mail->Body=$_POST('message');
+        $mail->setFrom('office.consoleghana@gmail.com'); // Use environment variable for sender email
+        $mail->addAddress($_POST['email']);
+        $mail->isHTML(true);
 
-$mail->send();
-        echo
-         "
-         <script>
-        alert('Your message was sent!');
-        document.location.href='index.html';
-        </script>";
+        $mail->Subject = $_POST['subject'];
+        $mail->Body = $_POST['message'];
 
-//     $to = "rfkabubakari@gmail.com"; // Your email address
-//     $name = $_POST['name'];
-//     $from = $_POST['email'];
-//     $message = $_POST['message'];
-//     $subject = "Contact Form Submission";
-
-//     $headers = "From: " . $from . "\r\n";
-//     $headers .= "Reply-To: " . $from . "\r\n";
-//     $headers .= "X-Mailer: PHP/" . phpversion();
-
-//     $result = mail($to, $subject, $message, $headers);
-
-//     if ($result)
-//     {
-//         echo '<script type="text/javascript">alert("Your message was sent!");</script>';
-//         echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
-
-//     }
-//     else
-//     {
-//         echo '<script type="text/javascript">alert("Oops! Your message wasn’t sent. Try again later.");</script>';
-//         echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
-//     }
+        if ($mail->send()) {
+            header('Location: index.html?success=true'); // Redirect with success flag
+        } else {
+            echo 'Error sending email: ' . $mail->ErrorInfo;
+        }
+    } catch (Exception $e) {
+        echo 'Error: ' . $e->getMessage();
+    }
 }
 ?>

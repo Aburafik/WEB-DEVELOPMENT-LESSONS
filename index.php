b727ab7d5406dfdec80a4183c54cdb0ce4bef335
Rafik <rfkabubakari@gmail.com>
2024-03-02 11:38:15 +0000

    new changes for php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Contact Form</title>
+</head>
+<body>
+    <form action="send.php" method="post">
+       
+       Email <input type="email" name="email" id="email" value="">
+        <br>
+        Subject <input type="text" name="subject" id="email" value="">
+        <br>
+        Message <input type="text" name="message" id="email" value=""><br>
+        <button type="submit" name="send">Send</button>
+        <br><br>
+    </form>
+</body>
+</html>
--- send.php
+++ send.php
@@ -1,39 +1,38 @@
-
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\PHPmailer;
 use PHPMailer\PHPMailer\Exception;
-
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPmailer.php';
 require 'phpmailer/src/SMTP.php';
+if (isset($_POST['send']))
+{
+$mail= new PHPMailer(true);
+$mail->isSMTP();
+$mail->Host=smtp.gmail.com;
+$mail->SMTPAuth=true;
+$mail->Username='office.consoleghana@gmail.com';
+$mail->password='swcd juwy wlyf wtrl';
+$mail->SMTPSecure='ssl';
+$mail->PORT='465';
 
-if (isset($_POST['send'])) {
-    $mail = new PHPMailer(true);
 
-    try {
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'office.consoleghana@gmail.com'; // Use environment variable for username
-        $mail->Password = 'swcd juwy wlyf wtrl'; // Use environment variable for password
-        $mail->SMTPSecure = 'ssl';
-        $mail->Port = 465;
+$mail->setFrom('office.consoleghana@gmail.com');
+$mail->AddAddress($_POST['email']);
+$mail->isHTML(true);
 
-        $mail->setFrom('office.consoleghana@gmail.com'); // Use environment variable for sender email
-        $mail->addAddress($_POST['email']);
-        $mail->isHTML(true);
+$mail->Subject=$_POST('subject');
+$mail->Body=$_POST('message');
 
-        $mail->Subject = $_POST['subject'];
-        $mail->Body = $_POST['message'];
+$mail->send();
+        echo
+         "
+         <script>
+        alert('Your message was sent!');
+        document.location.href='index.php';
+        </script>";
 
-        if ($mail->send()) {
-            header('Location: index.html?success=true'); // Redirect with success flag
-        } else {
-            echo 'Error sending email: ' . $mail->ErrorInfo;
-        }
-    } catch (Exception $e) {
-        echo 'Error: ' . $e->getMessage();
-    }
+
 }
 ?>
+
